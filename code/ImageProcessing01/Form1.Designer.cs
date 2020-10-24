dbfc9ec4dd0e55ab6c4314723d7b4d9344b6ecdd
giangnv125 <38546697+giangnv125@users.noreply.github.com>
2020-10-24 23:01:43 +0700

    first commit

---
+++ code/ImageProcessing01/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace ImageProcessing01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        /// 
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem basicToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oCRToolStripMenuItem;
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.basicToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oCRToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.chartRectY = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chartRectX = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.ptbLoad = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.nudDegree = new System.Windows.Forms.NumericUpDown();
+            this.lblPointChecker = new System.Windows.Forms.TextBox();
+            this.lblRotation = new System.Windows.Forms.TextBox();
+            this.btnOCR = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.plotView2 = new OxyPlot.WindowsForms.PlotView();
+            this.plotView1 = new OxyPlot.WindowsForms.PlotView();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.txtOCR = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartRectY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartRectX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbLoad)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDegree)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.basicToolStripMenuItem,
+            this.oCRToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1047, 24);
+            this.menuStrip1.TabIndex = 403;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // basicToolStripMenuItem
+            // 
+            this.basicToolStripMenuItem.Name = "basicToolStripMenuItem";
+            this.basicToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.basicToolStripMenuItem.Text = "Basic";
+            this.basicToolStripMenuItem.Click += new System.EventHandler(this.basicToolStripMenuItem_Click);
+            // 
+            // oCRToolStripMenuItem
+            // 
+            this.oCRToolStripMenuItem.Name = "oCRToolStripMenuItem";
+            this.oCRToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.oCRToolStripMenuItem.Text = "OCR";
+            this.oCRToolStripMenuItem.Click += new System.EventHandler(this.oCRToolStripMenuItem_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoad.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnLoad.Location = new System.Drawing.Point(91, 63);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(105, 37);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load Picture";
+            this.btnLoad.UseVisualStyleBackColor = false;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // ptbLoad
+            // 
+            this.ptbLoad.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.ptbLoad.Location = new System.Drawing.Point(248, 3);
+            this.ptbLoad.Name = "ptbLoad";
+            this.ptbLoad.Size = new System.Drawing.Size(1000, 800);
+            this.ptbLoad.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.ptbLoad.TabIndex = 0;
+            this.ptbLoad.TabStop = false;
+            this.ptbLoad.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ptbLoad_MouseDown);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(256, 11);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1000, 800);
+            this.panel1.TabIndex = 401;
+            this.panel1.Controls.Add(this.ptbLoad);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnPlay.Location = new System.Drawing.Point(92, 146);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(105, 37);
+            this.btnPlay.TabIndex = 404;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = false;
+            // 
+            // nudDegree
+            // 
+            this.nudDegree.Location = new System.Drawing.Point(92, 203);
+            this.nudDegree.Maximum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            0});
+            this.nudDegree.Name = "nudDegree";
+            this.nudDegree.Size = new System.Drawing.Size(105, 20);
+            this.nudDegree.TabIndex = 405;
+            // 
+            // lblPointChecker
+            // 
+            this.lblPointChecker.Location = new System.Drawing.Point(92, 242);
+            this.lblPointChecker.Name = "lblPointChecker";
+            this.lblPointChecker.Size = new System.Drawing.Size(105, 20);
+            this.lblPointChecker.TabIndex = 406;
+            // 
+            // lblRotation
+            // 
+            this.lblRotation.Location = new System.Drawing.Point(92, 284);
+            this.lblRotation.Name = "lblRotation";
+            this.lblRotation.Size = new System.Drawing.Size(105, 20);
+            this.lblRotation.TabIndex = 407;
+            // 
+            // btnOCR
+            // 
+            this.btnOCR.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnOCR.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOCR.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnOCR.Location = new System.Drawing.Point(85, 120);
+            this.btnOCR.Name = "btnOCR";
+            this.btnOCR.Size = new System.Drawing.Size(105, 37);
+            this.btnOCR.TabIndex = 404;
+            this.btnOCR.Text = "OCR";
+            this.btnOCR.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.ptbLoad);
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Controls.Add(this.btnLoad);
+            this.panel2.Controls.Add(this.menuStrip1);
+            this.panel2.Controls.Add(this.plotView2);
+            this.panel2.Controls.Add(this.plotView1);
+            this.panel2.Location = new System.Drawing.Point(11, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(2000, 659);
+            this.panel2.TabIndex = 404;
+            // 
+            // plotView2
+            // 
+            this.plotView2.Location = new System.Drawing.Point(259, 438);
+            this.plotView2.Name = "plotView2";
+            this.plotView2.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotView2.Size = new System.Drawing.Size(731, 157);
+            this.plotView2.TabIndex = 405;
+            this.plotView2.Text = "plotView2";
+            this.plotView2.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotView2.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotView2.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // plotView1
+            // 
+            this.plotView1.Location = new System.Drawing.Point(1000, 30);
+            this.plotView1.Name = "plotView1";
+            this.plotView1.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotView1.Size = new System.Drawing.Size(222, 400);
+            this.plotView1.TabIndex = 404;
+            this.plotView1.Text = "plotView1";
+            this.plotView1.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotView1.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotView1.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(262, 431);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(519, 199);
+            this.listBox.TabIndex = 404;
+            // 
+            // txtOCR
+            // 
+            this.txtOCR.Location = new System.Drawing.Point(92, 279);
+            this.txtOCR.Multiline = true;
+            this.txtOCR.Name = "txtOCR";
+            this.txtOCR.Size = new System.Drawing.Size(129, 83);
+            this.txtOCR.TabIndex = 406;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1070, 671);
+            this.Controls.Add(this.panel2);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartRectY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartRectX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbLoad)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDegree)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartRectY;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartRectX;
+        private System.Windows.Forms.PictureBox ptbLoad;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.NumericUpDown nudDegree;
+        private System.Windows.Forms.TextBox lblPointChecker;
+        private System.Windows.Forms.TextBox lblRotation;
+        private System.Windows.Forms.Button btnOCR;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListBox listBox;
+        private OxyPlot.WindowsForms.PlotView plotView2;
+        private OxyPlot.WindowsForms.PlotView plotView1;
+        private System.Windows.Forms.TextBox txtOCR;
+    }
+}
+
---
+++ code/ImageProcessing01/Form1.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+using System.IO;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.CV.ML;
+using Emgu.CV.ML.Structure;
+using Emgu.CV.OCR;
+using OxyPlot;
+using OxyPlot.Series;
+using Tesseract;
+
+namespace ImageProcessing01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap bitmap;
+        Image<Bgr, byte> imgIn;
+        System.Diagnostics.Stopwatch st = new System.Diagnostics.Stopwatch();
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Image files (*.jpg, *.jpeg, *.png) | *.jpg; *.jpeg; *.png";
+            if (open.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                bitmap = new Bitmap(open.FileName);
+                imgIn = new Image<Bgr, byte>(bitmap);
+                ptbLoad.Image = bitmap;
+
+            }
+        }
+
+        private Bitmap RotateBitmap(Bitmap b, float angle)
+        {
+            Bitmap bm = new Bitmap(b.Width, b.Height);
+            Graphics g = Graphics.FromImage(bm);
+            g.TranslateTransform((float)b.Width / 2, (float)b.Height / 2);
+            g.RotateTransform(angle);
+            g.TranslateTransform(-(float)b.Width / 2, -(float)b.Height / 2);
+            g.DrawImage(b, new Point(0, 0));
+            g.Dispose();
+            return bm;
+        }
+
+
+
+        private void nudDegree_ValueChanged(object sender, EventArgs e)
+        {
+            st.Reset();
+            st.Start();
+
+            float Degree = (float)nudDegree.Value;
+            ptbLoad.Image = RotateBitmap(bitmap, Degree);
+
+            st.Stop();
+            TimeSpan timespan = st.Elapsed;
+            lblRotation.Text = Convert.ToString(timespan.TotalMilliseconds + "\t ms");
+        }
+
+        private void convertGray(Bitmap bmpSource, Bitmap bmpNew)
+        {
+            BitmapData data24 = bmpSource.LockBits(new Rectangle(0, 0, bmpSource.Width, bmpSource.Height),
+                ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            BitmapData data8 = bmpNew.LockBits(new Rectangle(0, 0, bmpNew.Width, bmpNew.Height),
+                ImageLockMode.ReadWrite, PixelFormat.Format8bppIndexed);
+            int i, j;
+            unsafe
+            {
+                int pos24, pos8;
+                //int offset24 = data24.Stride - bitmap.Width * 3;
+                //int offset8=data8.Stride-bitmap.Width;
+                byte* p24 = (byte*)(data24.Scan0.ToPointer());
+                byte* p8 = (byte*)(data8.Scan0.ToPointer());
+                for (i = 0; i < data8.Height; i++)
+                {
+                    for (j = 0; j < data8.Width; j++)
+                    {
+                        pos8 = j + i * data8.Stride;
+                        pos24 = 3 * j + i * data24.Stride;
+                        p8[pos8] = (byte)((int)(p24[pos24] + p24[pos24 + 1] + p24[pos24 + 2]) / 3);
+                    }
+                    //p24 += offset24;
+                    //p8 += offset8;
+
+                }
+            }
+
+            bmpSource.UnlockBits(data24);
+            bmpNew.UnlockBits(data8);
+        }
+
+        unsafe
+        private void ptbLoad_MouseDown(object sender, MouseEventArgs e)
+        {
+            st.Reset();
+            st.Start();
+
+            int[,] gray = new int[bitmap.Width, bitmap.Height];
+            //Chuyển ảnh xám trực tiếp trên bitmap
+            BitmapData data = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                ImageLockMode.ReadWrite, bitmap.PixelFormat);
+            int offset = data.Stride - bitmap.Width * 3;
+            byte* p = (byte*)data.Scan0;
+
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    int t = (int)(p[0] + p[1] + p[2]) / 3; //Y = 0.2126 * R + 0.7152 * G + 0.0722 * B
+                    p[0] = p[1] = p[2] = (byte)t;
+
+                    gray[i, j] = t;
+                    p += 3;      //áp dụng cho ảnh jpg, jpeg 24bit, ảnh png p+=4 do có thành phần thể hiện độ trong suốt
+                }
+                p += offset;
+            }
+
+            bitmap.UnlockBits(data);
+            ptbLoad.Image = bitmap;
+
+            //Bitmap bmpNew = new Bitmap(bitmap.Width, bitmap.Height, PixelFormat.Format8bppIndexed);
+
+            //PixelFormat format = bitmap.PixelFormat;
+            //if (format == PixelFormat.Format24bppRgb)
+            //{
+            //    convertGray(bitmap, bmpNew);
+            //    ptbLoad.Image = bmpNew;
+            //}
+
+            int x = e.X;
+            int y = e.Y;
+
+            Graphics gfx = ptbLoad.CreateGraphics();
+            gfx.DrawLine(
+                new Pen(Color.Red, 3f),
+               new Point(x, 0),
+               new Point(x, y - 5));
+            gfx.DrawLine(
+                new Pen(Color.Red, 3f),
+               new Point(x, bitmap.Height),
+               new Point(x, y + 5));
+            gfx.DrawLine(
+                new Pen(Color.Red, 3f),
+               new Point(0, y),
+               new Point(x - 5, y));
+            gfx.DrawLine(
+               new Pen(Color.Red, 3f),
+              new Point(bitmap.Width, y),
+              new Point(x + 5, y));
+
+            PlotModel pm1 = new PlotModel();
+            OxyPlot.Axes.LinearAxis XAxis1 = new OxyPlot.Axes.LinearAxis
+            { Position = OxyPlot.Axes.AxisPosition.Bottom, Minimum = 0, Maximum = bitmap.Width };
+
+            OxyPlot.Axes.LinearAxis YAxis1 = new OxyPlot.Axes.LinearAxis
+            { Position = OxyPlot.Axes.AxisPosition.Left, Minimum = 0, Maximum = 255 };
+            pm1.Axes.Add(XAxis1);
+            pm1.Axes.Add(YAxis1);
+
+            var s1 = new LineSeries { Title = "rectX", StrokeThickness = 1 };
+
+            plotView2.Model = pm1;
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                s1.Points.Add(new DataPoint(i, gray[i, y]));
+            }
+
+            plotView2.Model.Series.Add(s1);
+
+            PlotModel pm2 = new PlotModel();
+            OxyPlot.Axes.LinearAxis XAxis2 = new OxyPlot.Axes.LinearAxis
+            { Position = OxyPlot.Axes.AxisPosition.Left, Minimum = 0, Maximum = 255 };
+
+            OxyPlot.Axes.LinearAxis YAxis2 = new OxyPlot.Axes.LinearAxis
+            { Position = OxyPlot.Axes.AxisPosition.Bottom, Minimum = 0, Maximum = bitmap.Height };
+
+            pm2.Axes.Add(XAxis2);
+            pm2.Axes.Add(YAxis2);
+
+            var s2 = new LineSeries { Title = "rectY", StrokeThickness = 1 };
+
+            plotView1.Model = pm2;
+            for (int i = 0; i < bitmap.Height; i++)
+            {
+                s2.Points.Add(new DataPoint(gray[x, i], i));
+            }
+
+            plotView1.Model.Series.Add(s2);
+
+            //for (int i=0; i<bitmap.Width; i++)
+            //{
+
+            //}
+
+            //drawGraph(chartRectX, x, gray[x, y], bitmap.Width);
+            //drawGraph(chartRectY, y, gray[x, y], bitmap.Height);
+
+            st.Stop();
+            TimeSpan timespan = st.Elapsed;
+            lblPointChecker.Text = Convert.ToString(timespan.TotalMilliseconds + "\t ms");
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            st.Reset();
+            st.Start();
+            string[] images = Directory.GetFiles(@"C:\Users\ADMIN\Downloads", "*.jpg");
+
+            foreach (string image in images)
+            {
+                Bitmap bm = new Bitmap(image);
+                Bitmap bmTemp = bm;
+                ptbLoad.Image = bmTemp;
+                bm.Dispose();
+                bm = null;
+                i++;
+
+            }
+            st.Stop();
+            TimeSpan timespan = st.Elapsed;
+            lblPointChecker.Text = Convert.ToString(timespan.TotalMilliseconds + "\t ms");
+            //lblRotation.Text = Convert.ToString(i);
+        }
+        private void btnOCR_Click(object sender, EventArgs e)
+        {
+            var ocr = new TesseractEngine("./tessdata", "eng", EngineMode.TesseractAndCube);
+
+            Image<Gray, byte> imgOut = imgIn.Convert<Gray, byte>().Not().ThresholdBinary(new Gray(50), new Gray(255));
+            VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+            Mat matImg = new Mat();
+
+            CvInvoke.FindContours(imgOut, contours, matImg, Emgu.CV.CvEnum.RetrType.External,
+                Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
+            //contours = FilterContours(contours, 500);
+            if (contours.Size > 0)
+            {
+                for (int i = 0; i < contours.Size; i++)
+                {
+                    Rectangle rect = CvInvoke.BoundingRectangle(contours[i]);
+                    imgIn.ROI = rect;
+
+                    listBox.Items.Add(rect.Location.X);
+                    listBox.Items.Add(rect.Location.Y);
+
+                    Bitmap img = imgIn.Copy().Bitmap;
+                    imgIn.Dispose();
+
+                    var temp = ocr.Process(img);
+                    img.Dispose();
+                    txtOCR.Text = temp.GetText();
+                    listBox.Items.Add(temp.GetText());
+                }
+            }
+        }
+
+        private void basicToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //this.oCRToolStripMenuItem.Click += new System.EventHandler(this.oCRToolStripMenuItem_Click);
+            panel2.Controls.Add(this.lblRotation);
+            panel2.Controls.Add(this.lblPointChecker);
+            MainMenuStrip = this.menuStrip1;
+            panel2.Controls.Add(nudDegree);
+            this.nudDegree.ValueChanged += new System.EventHandler(this.nudDegree_ValueChanged);
+            this.Controls.Add(this.panel1);
+            panel2.Controls.Add(this.btnPlay);
+            btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            panel2.Controls.Add(this.plotView2);
+            plotView2.SuspendLayout();
+            panel2.Controls.Add(this.plotView1);
+            plotView1.SuspendLayout();
+            panel2.Controls.Add(this.btnLoad);
+            btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            panel2.Controls.Add(plotView1);
+            //((System.ComponentModel.ISupportInitialize)(this.plotView1)).EndInit();
+            //((System.ComponentModel.ISupportInitialize)(this.plotView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbLoad)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDegree)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+            if (panel2.Controls.Contains(btnOCR))
+            {
+                this.btnOCR.Click -= new System.EventHandler(this.btnOCR_Click);
+                panel2.Controls.Remove(txtOCR);
+                txtOCR.Dispose();
+
+                panel2.Controls.Remove(btnOCR);
+                btnOCR.Dispose();
+            }
+        }
+
+        private void oCRToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //this.basicToolStripMenuItem.Click += new System.EventHandler(this.basicToolStripMenuItem_Click);
+            panel2.Controls.Add(this.btnOCR);
+            MainMenuStrip = this.menuStrip1;
+            this.panel2.Controls.Add(this.txtOCR);
+            panel2.Controls.Add(this.listBox);
+            btnOCR.Click += new System.EventHandler(this.btnOCR_Click);
+            if (panel2.Controls.Contains(btnPlay))
+            {
+                this.btnPlay.Click -= new System.EventHandler(this.btnPlay_Click);
+                panel2.Controls.Remove(btnPlay);
+                btnPlay.Dispose();
+
+                this.nudDegree.ValueChanged -= new System.EventHandler(this.nudDegree_ValueChanged);
+                panel2.Controls.Remove(nudDegree);
+                nudDegree.Dispose();
+
+                panel2.Controls.Remove(lblPointChecker);
+                lblPointChecker.Dispose();
+
+                panel2.Controls.Remove(lblRotation);
+                lblRotation.Dispose();
+
+                panel2.Controls.Remove(plotView1);
+               plotView1.Dispose();
+
+                panel2.Controls.Remove(plotView2);
+                plotView2.Dispose();
+            }           
+        }
+    }
+}
